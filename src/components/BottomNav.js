83dfea3755cfda8a3c09f2d760b0cfc4aaa478df
JGSimi <joao02simi@gmail.com>
2024-10-31 15:10:22 -0300

    a

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ed972b562a23f604a1305f9da66167af3cb896e1375da52b8c96396b1523920
-size 9707
+oid sha256:7f3d0d8c9886c096c589899c05dfe68f456fc8c5bfa5d788d6a50c42248e1170
+size 11017
